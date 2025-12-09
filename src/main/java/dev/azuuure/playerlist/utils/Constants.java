d65dcc19fc64196836ca411221b56de9018599cb
azurejelly <178000437+azurejelly@users.noreply.github.com>
2025-12-09 17:08:32 -0500

    refactor: add javadocs

--- src/main/java/dev/azuuure/playerlist/utils/Constants.java
+++ src/main/java/dev/azuuure/playerlist/utils/Constants.java
@@ -7,6 +7,7 @@ public final class Constants {
     private Constants() {
         throw new UnsupportedOperationException();
     }
+
     /**
      * The ID of the mod.
      */
@@ -28,10 +29,39 @@ public final class Constants {
      */
     public static final String CONFIGURATION_FILE = MOD_ID + ".properties";
 
+    /**
+     * A lime green color in hexadecimal, which is used for very good
+     * network connections.
+     */
     public static final int COLOR_EXCELLENT = 0xA1FF30;
+
+    /**
+     * A dark green color in hexadecimal, which is used for good
+     * network connections.
+     */
     public static final int COLOR_GOOD = 0x1C9115;
+
+    /**
+     * A yellow color in hexadecimal, which is used for network
+     * connections that are good enough.
+     */
     public static final int COLOR_DECENT = 0xFCE956;
+
+    /**
+     * A red color in hexadecimal, which is used for bad network
+     * connections.
+     */
     public static final int COLOR_BAD = 0xFD4B4B;
+
+    /**
+     * A very vibrant red color in hexadecimal, used for very bad
+     * network connections.
+     */
     public static final int COLOR_VERY_BAD = 0xFF0000;
+
+    /**
+     * A dark red color in hexadecimal. How are you even connected
+     * still?
+     */
     public static final int COLOR_HORRIBLE = 0x8F0000;
 }
