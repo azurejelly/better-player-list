8f234105a2b9c7ef266f0df2a5b2fb0d1da22a82
azurejelly <178000437+azurejelly@users.noreply.github.com>
2026-01-02 05:02:08 -0500

    fix(fabric): fix duplicates inside final fabric JAR

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -18,9 +18,6 @@ loom {
 }
 
 dependencies {
-    include(project(":common"))
-    implementation(project(":common"))
-
     minecraft(libs.minecraft)
     mappings(
         variantOf(libs.fabric.yarn) {
@@ -28,26 +25,34 @@ dependencies {
         }
     )
 
+    modImplementation(project(":common"))
     modImplementation(libs.fabric.loader)
     modImplementation(libs.fabric.api)
     modImplementation(libs.modmenu)
     modRuntimeOnly(libs.devauth.fabric)
 }
 
-tasks.processResources {
-    filteringCharset = "UTF-8"
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+tasks {
+    jar {
+        from(sourceSets["main"].output)
+        from(project(":common").sourceSets["main"].output.classesDirs)
+    }
 
-    from(project(":common").sourceSets.main.get().resources)
-    filesMatching("fabric.mod.json") {
-        expand(
-            "version" to project.version,
-            "minecraft_version" to libs.versions.minecraft.get(),
-            "loader_version" to libs.versions.fabric.loader.get(),
-            "mod_id" to rootProject.property("mod_id").toString(),
-            "mod_name" to rootProject.property("mod_name").toString(),
-            "mod_description" to rootProject.property("mod_description").toString(),
-            "license" to rootProject.property("license").toString()
-        )
+    processResources {
+        filteringCharset = "UTF-8"
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+        from(project(":common").sourceSets.main.get().resources)
+        filesMatching("fabric.mod.json") {
+            expand(
+                "version" to project.version,
+                "minecraft_version" to libs.versions.minecraft.get(),
+                "loader_version" to libs.versions.fabric.loader.get(),
+                "mod_id" to rootProject.property("mod_id").toString(),
+                "mod_name" to rootProject.property("mod_name").toString(),
+                "mod_description" to rootProject.property("mod_description").toString(),
+                "license" to rootProject.property("license").toString()
+            )
+        }
     }
 }
