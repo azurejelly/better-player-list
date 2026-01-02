f0a0baea12111dc1970f7d391717d362cf9d50d0
azurejelly <178000437+azurejelly@users.noreply.github.com>
2026-01-02 04:43:58 -0500

    refactor(neoforge): use lombok's @UtilityClass annotation instead

--- neoforge/src/main/java/dev/azuuure/playerlist/neoforge/utils/NeoForgeConstants.java
+++ neoforge/src/main/java/dev/azuuure/playerlist/neoforge/utils/NeoForgeConstants.java
@@ -1,13 +1,11 @@
 package dev.azuuure.playerlist.neoforge.utils;
 
 import dev.azuuure.playerlist.utils.Constants;
+import lombok.experimental.UtilityClass;
 import net.neoforged.fml.ModList;
 
-public final class NeoForgeConstants {
-
-    private NeoForgeConstants() {
-        throw new UnsupportedOperationException();
-    }
+@UtilityClass
+public class NeoForgeConstants {
 
     public static final String MOD_VERSION = ModList.get()
             .getModFileById(Constants.MOD_ID)
